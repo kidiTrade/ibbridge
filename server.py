3f9ce17975b7ee2e9a46b84f842799c44ca58599
Arnaud Rebts <arnaud.rebts@gmail.com>
2020-02-04 15:06:23 +0100

    feat: rename ib_loader to ib_bridge
    BREAKING CHANGE

--- server.py
+++ server.py
@@ -8,8 +8,8 @@ from google.protobuf.timestamp_pb2 import Timestamp
 from grpclib.server import Server, Stream
 from grpclib.utils import graceful_exit
 from ib_insync import IB, Stock
-from ib_loader_grpc import IbLoaderBase
-from ib_loader_pb2 import Bar, GetStockHistoricalDataRequest
+from ibbridge_grpc import IbLoaderBase
+from ibbridge_pb2 import Bar, GetStockHistoricalDataRequest
 
 
 def timestamp_from_datetime(dt):
